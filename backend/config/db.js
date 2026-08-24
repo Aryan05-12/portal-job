f0d0d7d91d00cce65585b598381fe168b14cd4a9
Aryan <aryan120507143@gmail.com>
2026-08-24 22:27:28 +0530

    Updated MongoDB connection to Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const ConnectDB = async () => {
 
     try{
 
-       await mongoose.connect('mongodb://localhost:27017/job')
+       await mongoose.connect(process.env.MONGO_URI)
         console.log(" Congratulation! MongoDB is Connected and Working")
     }catch(error){
             console.log("Sorry MongoDb is Disconnected",error)
