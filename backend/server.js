5374ebb4f2cefce1520687790f2225f877efafdf
Aryan <aryan120507143@gmail.com>
2026-08-24 22:48:21 +0530

    added fullstack deployment setup

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express()
 require('dotenv').config()
 const dbconnection = require('./config/db')
 const cors = require('cors')
+const path = require('path');
 
 
 console.log("EMAIL_USER:", process.env.EMAIL_EMAIL);
@@ -14,6 +15,13 @@ app.use(express.urlencoded({ extended: true, limit: '100mb' }))
 
 dbconnection()
 app.use('/api', require('./routes/AuthRoute'))
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'))
+})
+
 
 const PORT = parseInt(process.env.PORT, 10) || 1111
 app.listen(PORT, ()=>console.log("server is ready"))
